eb7bc734b60256ff14fad6ad3249d938fbe5e7c7
TiUmarovR <TiUmarov.R@gmail.com>
2023-11-08 12:58:38 +0300

    now slider work as i want to

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,11 @@ const slider = document.querySelector('.swiper')
 
 let mySwiper = new Swiper(slider, {
     direction: 'horizontal',
+    spaceBetween: 10,
+    loop: false,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
 });
